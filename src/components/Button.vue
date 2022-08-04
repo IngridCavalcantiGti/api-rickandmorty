2db62926a98c635362e03f2f115d494879375e11
Ingrid <ingridgti08@hotmail.com>
2022-08-03 23:33:29 -0300

    cria componente button e adiciona na Home

---
+++ src/components/Button.vue
@@ -0,0 +1,41 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <div class="flex space-x-2 justify-center">
+    <button
+      @click="() => $emit('onClick')"
+      type="button"
+      class="
+        inline-block
+        px-6
+        py-2.5
+        bg-blue-600
+        text-white
+        font-medium
+        text-xs
+        leading-tight
+        uppercase
+        rounded
+        shadow-md
+        hover:bg-blue-700 hover:shadow-lg
+        focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+        active:bg-blue-800 active:shadow-lg
+        transition
+        duration-150
+        ease-in-out
+      "
+    >
+      {{ label }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    label: {
+      type: String,
+    },
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="bg-gray-400">
+    <div class="flex justify-end">
+      <Button label="Status Alive" class="m-2" />
+      <Button label="Species Human" class="m-2" />
+      <Button label="Grouping by status" class="m-2" />
+    </div>
+
     <Search @onInput="onInput" />
     <div class="flex flex-wrap">
       <Card :characters="characters" class="border-gray-400 border-4"></Card>
@@ -12,9 +18,10 @@ import { defineComponent, ref, onMounted } from "vue";
 import axios from "axios";
 import Card from "@/components/Card.vue";
 import Search from "@/components/Search.vue";
+import Button from "@/components/Button.vue";
 
 export default defineComponent({
-  components: { Card, Search },
+  components: { Card, Search, Button },
   setup() {
     const characters = ref(null);
 
